1ff588f1df4d99e3442acf02a7780cb19653af5b
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2023-06-15 09:21:48 +0000

    probando cosas

--- src/app.js
+++ src/app.js
@@ -5,7 +5,25 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+window.onLoad = function() {
+  // Generar los nombres de dominio
+  let pronoun = ["the", "our"];
+  let adj = ["great", "big"];
+  let noun = ["jogger", "racoon", "mouse"];
+  let extension = [".com", ".net", ".pt", ".ly", ".es", ".org"];
+  let domains = "";
+
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        for (let l = 0; l < extension.length; l++) {
+          let domain = pronoun[i] + adj[j] + noun[k] + extension[l];
+          domains += "<ol>" + domain + "</ol>";
+        }
+      }
+    }
+  }
+  console.log(domains);
+  let domainList = document.querySelector("#domainList");
+  domainList.innerHTML = domains;
 };
