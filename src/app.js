f1a18a1932e3e0bd1aa5b87fdcd17818cdbd9592
Ayoze Gómez Rosa <130746651+ayozegr05@users.noreply.github.com>
2023-06-19 18:31:57 +0000

    Finished revision project

--- src/app.js
+++ src/app.js
@@ -1,35 +1,36 @@
 /* eslint-disable */
 import "bootstrap";
 import "./style.css";
-
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  // Generar los nombres de dominio
-  let pronoun = ["the", "our"];
-  let adj = ["great", "big"];
-  let noun = ["jogger", "racoon", "mouse"];
-  let extension = [".com", ".net", ".pt", ".ly", ".es", ".org"];
+  
+  function generateRandomDomain(pronouns, adjetives, nouns, extensions) {
+    let domains = "<h1 class = 'text-center'>Domain Generator</h1><div class = 'd-flex justify-content-center'><ol class = 'mt-5 list-group-numbered'>";
 
-  function dominio1() {
-    let domains = "<h1 class='text-center'>Domain Generator</h1><div class='d-flex justify-content-center'><ol class='mt-5 list-group-numbered'>";
-
-    for (let i = 0; i < pronoun.length; i++) {
-      for (let j = 0; j < adj.length; j++) {
-        for (let k = 0; k < noun.length; k++) {
-          for (let l = 0; l < extension.length; l++) {
-            let domain = pronoun[i] + adj[j] + noun[k] + extension[l];
-            domains += `<li class="list-group-item">${domain}</li>`;
+    for (let i = 0; i < pronouns.length; i++) {
+      for (let j = 0; j < adjetives.length; j++) {
+        for (let k = 0; k < nouns.length; k++) {
+          for (let l = 0; l < extensions.length; l++) {
+            let domain = pronouns[i] + adjetives[j] + nouns[k] + extensions[l];
+            domains += `<li class = "list-group-item">${domain}</li>`;
           }
         }
       }
     }
+
     domains += "</ol></div>";
     return domains;
   }
-  // console.log(dominio1());
 
+  // Generar los nombres de dominio
+  let myPronouns = ["the", "our"];
+  let myAdjetives = ["great", "big"];
+  let myNouns = ["jogger", "racoon", "mouse"];
+  let myExtensions = [".com", ".net", ".pt", ".ly", ".es", ".org"];
+ 
+  //Vinculamos nuestro archivo js
   let domainList = document.querySelector("#domainList");
-  domainList.innerHTML = dominio1();
+  domainList.innerHTML = generateRandomDomain(myPronouns, myAdjetives, myNouns, myExtensions);
 };
